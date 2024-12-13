5e22b829ced98900e7d2ad3b10130ae9fe17a8c9
Your Name <whatGravity@protonmail.com>
2024-12-13 15:46:57 -0600

    day 11

--- 2024_python-solutions/11.py
+++ 2024_python-solutions/11.py
@@ -1,52 +1,17 @@
+from sys import argv
+from functools import cache
 from aoc import get_input
 
-def count_digits(n):
-    # Faster way to count digits without string conversion
-    if n == 0:
-        return 1
-    count = 0
-    while n:
-        count += 1
-        n //= 10
-    return count
+@cache
+def count_stones(number: str, depth: int) -> int:
+    if depth==0: return 1
+    if number=="0" or number=="": return count_stones("1", depth-1)
+    if len(number)%2: return count_stones(str(int(number)*2024), depth-1)
+    l = len(number)//2
+    return count_stones(number[:l], depth-1)+count_stones(number[l:].lstrip("0"), depth-1)
 
-def solution(data, blinks):
-    # Convert data to list if it isn't already
-    data = list(data)
-    
-    for _ in range(1, blinks+1):
-        i = 0
-        while i < len(data):
-            num = data[i]
-            
-            # Use direct integer comparison instead of string conversion for zero check
-            if num == 0:
-                data[i] = 1
-            else:
-                # Calculate even/odd length without string conversion
-                num_length = count_digits(num)
-                
-                if num_length % 2 == 0:
-                    # Split number using integer division
-                    divisor = 10 ** (num_length // 2)
-                    right_half = num % divisor
-                    left_half = num // divisor
-                    data[i] = left_half
-                    data.insert(i + 1, right_half)
-                    i += 1
-                else:
-                    data[i] = num * 2024
-            i += 1
-            
-        if _ % 2 == 0:  # Only print every 10th blink to reduce I/O
-            print(f"blink: {_}")
-    
-    return data
 
-def parse_input(input_str):
-    return [int(num) for num in input_str.split()]
 
-if __name__ == "__main__":
-    data = parse_input(get_input(11))
-    print(len(solution(data,25)))
-
+data = get_input(11)
+print(sum(count_stones(s, 25) for s in data.split()))
+print(sum(count_stones(s, 75) for s in data.split()))
